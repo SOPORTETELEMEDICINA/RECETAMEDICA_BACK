5f5b7984f606acc64a1e23a73260b13adbe26349
GustavoB <guz_bonilla@hotmail.com>
2025-08-20 14:17:14 -0600

    subir onesignal

---
+++ RMD.OneSignal/Models/ScheduleOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace RMD.OneSignal.Models;
+
+public sealed class ScheduleOptions
+{
+    // Envío inmediato si es null
+    public DateTimeOffset? SendAfter { get; set; }
+
+    // Para “paquete del día”: varias horas en el día
+    public IEnumerable<DateTimeOffset>? MultipleTimes { get; set; }
+
+    // Timezone OneSignal
+    public string? TimeZone { get; set; }
+}
---
+++ RMD.OneSignal/Service/Interfaces/INotificationsService.cs
@@ -0,0 +1,15 @@
+﻿using RMD.OneSignal.Models;
+
+namespace RMD.OneSignal.Service.Interfaces;
+
+public interface INotificationsService
+{
+    // Envío inmediato a un usuario
+    Task SendNowAsync(NotificationRequest req);
+
+    // Envío programado a un usuario (una o varias horas)
+    Task ScheduleAsync(NotificationRequest req, ScheduleOptions schedule);
+
+    // “Paquete del día”: mandar varias notificaciones a varios pacientes
+    Task SendDailyPackageAsync(IEnumerable<string> externalUserIds, IEnumerable<(string title, string body, DateTimeOffset at)> alerts, string? timeZone = null);
+}
